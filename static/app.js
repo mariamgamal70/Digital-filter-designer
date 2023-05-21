06ddb0a37758aa7aee876687ce5ac58cdcabfda7
AbdulmonemElsherif <83321464+AbdulmonemElsherif@users.noreply.github.com>
2023-05-21 20:50:14 +0300

    freq rrsponse

--- static/app.js
+++ static/app.js
@@ -5,6 +5,50 @@ const outputSignalGraph = document.getElementById("outputsignal");
 const graphSpeed = document.getElementById("speed");
 const uploadSignal = document.getElementById("uploadsignal");
 let time = 50;
+const Complex = require("complex.js");
+const Fraction = require("fraction.js");
+
+// Function to calculate the frequency response of a filter given its zeros and poles
+function calculateFrequencyResponse(zeros, poles) {
+  // Calculate the frequency response at each frequency
+  const frequencies = [];
+  const magnitudes = [];
+  const phases = [];
+  for (let i = 0; i < 1000; i++) {
+    const frequency = i / 1000;
+
+    // Calculate the numerator and denominator of the transfer function
+    let numerator = new Complex(1, 0);
+    let denominator = new Complex(1, 0);
+    for (let j = 0; j < zeros.length; j++) {
+      numerator = numerator.mul(
+        new Complex(1, 0).sub(new Complex(zeros[j], 0).mul(frequency))
+      );
+    }
+    for (let j = 0; j < poles.length; j++) {
+      denominator = denominator.mul(
+        new Complex(1, 0).sub(new Complex(poles[j], 0).mul(frequency))
+      );
+    }
+    // Calculate the transfer function
+    const transferFunction = numerator.div(denominator);
+    
+    // Calculate the magnitude and phase of the transfer function
+    const magnitude = transferFunction.abs();
+    const phase = transferFunction.arg();
+    
+    // Add the frequency, magnitude and phase to the respective arrays
+    frequencies.push(frequency);
+    magnitudes.push(magnitude);
+    phases.push(phase);
+  }
+  return { frequencies, magnitudes, phases };
+}
+
+const unitCircleCanvas = document.getElementById("unitcirclecanva");
+plotFilterResponse(zeros, poles, unitCircleCanvas);
+
+
 
 window.addEventListener("load", function () {
   createPlot(magnitudeGraph);
@@ -134,3 +178,46 @@ uploadSignal.addEventListener("change", (event) => {
   };
 });
 
+
+function plotFilterResponse(zeros, poles, unitCircleCanvas) {
+  const { frequencies, magnitudes, phases } = calculateFrequencyResponse(zeros, poles);
+
+  // Plot Magnitude
+  const magnitudeTrace = {
+    x: frequencies,
+    y: magnitudes,
+    mode: "lines",
+  };
+  Plotly.update(magnitudeGraph, { data: [magnitudeTrace] });
+
+  // Plot Phase
+  const phaseTrace = {
+    x: frequencies,
+    y: phases,
+    mode: "lines",
+  };
+  Plotly.update(phaseGraph, { data: [phaseTrace] });
+
+  // Plot zeros and poles on the unit circle
+  const ctx = unitCircleCanvas.getContext("2d");
+  ctx.clearRect(0, 0, unitCircleCanvas.width, unitCircleCanvas.height);
+  ctx.beginPath();
+  ctx.arc(unitCircleCanvas.width / 2, unitCircleCanvas.height / 2, unitCircleCanvas.width / 2 - 1, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "blue";
+  zeros.forEach((zero) => {
+    const x = zero.re * unitCircleCanvas.width / 2 + unitCircleCanvas.width / 2;
+    const y = -zero.im * unitCircleCanvas.height / 2 + unitCircleCanvas.height / 2;
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+  });
+  ctx.fillStyle = "red";
+  poles.forEach((pole) => {
+    const x = pole.re * unitCircleCanvas.width / 2 + unitCircleCanvas.width / 2;
+    const y = -pole.im * unitCircleCanvas.height / 2 + unitCircleCanvas.height / 2;
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+  });
+}
